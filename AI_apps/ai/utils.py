d4fb1ff9101dbc3bd32bd292d82be597d643097e
DAMeKz <hwanseo6417@naver.com>
2022-07-30 21:06:21 +0900

    [*] fix

--- AI_apps/ai/utils.py
+++ AI_apps/ai/utils.py
@@ -15,26 +15,3 @@ def get_prediction(model,text):
 def get_predict_proba(model,text):
     results = model.predict_proba([text])
     return results
-
-def get_pred_prob_bar(pred_prob):
-    if platform.system()=='Windows': # Windows
-        plt.rc('font', family='NanumMyeongjo')  
-    elif platform.system() == 'Darwin': # MAC 
-        plt.rc('font', family = 'AppleGothic') 
-    
-    plt.rc('font', size = 12)
-    plt.rc('axes', unicode_minus = False) 
-
-    prob_ind = ['Bias', 'Pure', 'Gender bias', 'Offensive', 'Gender offensive']
-    
-    plt.figure(figsize=(10,7))
-    res_prob_bar = plt.bar(prob_ind,pred_prob[0],color = COLORS)
-    plt.ylim([0,1])
-    plt.title('Result Probabilities')
-
-    for num in res_prob_bar:
-        height = num.get_height()
-        plt.text(num.get_x() + num.get_width()/2.0, height, '%.1f' % (height*100), ha='center', va='bottom', size = 12)
-
-    plt.show()
-
